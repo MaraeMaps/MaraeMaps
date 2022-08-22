179f99e4d41f288d10d136d0c120c20464cb3b14
chaka160 <kchay@windowslive.com>
2022-08-23 00:24:07 +1200

    Trying to get nav working
    TODO:
    - Fragments seem to not be going through transactions correctly. Need to revise.
    - Search seems to not work
    - Make choices of settings be visible to main activity, and change locale of app/font size of app.

--- app/src/main/java/com/example/maps/ui/home/MapFragment.kt
+++ app/src/main/java/com/example/maps/ui/home/MapFragment.kt
@@ -30,7 +30,6 @@ class MapFragment : Fragment() {
          * user has installed Google Play services and returned to the app.
          */
         val sydney = LatLng(-34.0, 151.0)
-        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 
