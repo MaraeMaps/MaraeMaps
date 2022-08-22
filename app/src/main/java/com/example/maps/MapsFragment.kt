0dfc1f96bf898fac00d15176cb2e013fb26fb3cc
lucysladden <lj.sladden@gmail.com>
2022-08-22 18:49:57 +1200

    got pop-ups working on harrys branch.

--- app/src/main/java/com/example/maps/MapsFragment.kt
+++ app/src/main/java/com/example/maps/MapsFragment.kt
@@ -13,6 +13,7 @@ import android.system.Os.open
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.FragmentActivity
 import com.example.maps.R
 import com.example.maps.core.Marae
@@ -41,9 +42,9 @@ import kotlin.reflect.typeOf
 //private val Parcelable.Name: String
 //    get() {return "Test Name Marae"}
 
-class MapsFragment : Fragment() {
+class MapsFragment : Fragment(), GoogleMap.OnMarkerClickListener,GoogleMap.InfoWindowAdapter  {
 
-
+    private var myContentsView: View? = null
 
     private val callback = OnMapReadyCallback { googleMap ->
         /**
@@ -55,6 +56,7 @@ class MapsFragment : Fragment() {
          * install it inside the SupportMapFragment. This method will only be triggered once the
          * user has installed Google Play services and returned to the app.
          */
+        myContentsView = layoutInflater.inflate(R.layout.popup, null);
 
         // Create a Marker array and iterate through marae to add them to the map
         var mMarkers: java.util.ArrayList<Marker> = java.util.ArrayList()
@@ -64,13 +66,16 @@ class MapsFragment : Fragment() {
 
         val pos = LatLng(maraeArray[0].Y, maraeArray[0].X)
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(pos))
+        googleMap.setInfoWindowAdapter(this)
+
 
         if (maraeArray != null){
             for (marae in maraeArray) {
                 val LL = LatLng(marae.Y, marae.X)
-                mMarkers.add(
-                    googleMap.addMarker(MarkerOptions().position(LL).title(marae.Name))
-            )
+                val marker: Marker = googleMap.addMarker(MarkerOptions().position(LL).title(marae.Name))
+                marker.tag = marae
+                println(marker.tag.toString())
+                mMarkers.add(marker)
         }
         }
 
@@ -89,5 +94,40 @@ class MapsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
+    }
+
+    override fun onMarkerClick(p0: Marker): Boolean {
+
+        TODO("Not yet implemented")
+    }
+
+    override fun getInfoWindow(p0: Marker): View? {
+        return null
+    }
+
+    override fun getInfoContents(p0: Marker): View? {
+        val ma: Marae = p0.tag as Marae
+        val iwi = myContentsView?.findViewById<TextView>(com.example.maps.R.id.iwi)
+        val title = myContentsView?.findViewById<TextView>(com.example.maps.R.id.title)
+        val region = myContentsView?.findViewById<TextView>(com.example.maps.R.id.region)
+        val location = myContentsView?.findViewById<TextView>(com.example.maps.R.id.location)
+        if (iwi != null) {
+            if (ma.Iwi == ""){
+                iwi.text = "Iwi information not available"
+            } else {
+                iwi.text = "Iwi: " + ma.Iwi
+            }
+        }
+        if (title != null) {
+            title.text = ma.Name
+        }
+        if (region != null) {
+            region.text = "Region: " + ma.TPK_Region
+        }
+        if (location != null) {
+            location.text = "Address: " + ma.Location
+        }
+        return myContentsView
+
     }
 }
