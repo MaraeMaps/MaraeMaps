7e931575e06c207021e044d13c7ccabcf415007e
Hugo Phibbs <81545721+HugoPhibbs@users.noreply.github.com>
2022-08-23 13:56:14 +1200

    changes to Marae
    adding docs

--- app/src/main/java/com/example/maps/core/Marae.kt
+++ app/src/main/java/com/example/maps/core/Marae.kt
@@ -1,30 +1,38 @@
 package com.example.maps.core
 
 /**
- * Data class for a Marae class
+ * Data class for a Marae
  *
  * @author Lucy Sladden
  */
 data class Marae(
-    val Alternate: Any,
-    val Comments: Any,
-    val Feedback: String,
-    val GIS_MID: Int,
-    /* Link to google maps for this Marae*/
+
+    /* Link to Google Maps for this Marae*/
     val GoogleMaps: String,
+    /* String that this Marae belongs to */
     val Hapu: String,
+    /* String that this Marae belongs to */
     val Iwi: String,
     /* Location of this Marae, ie address*/
     val Location: String,
+    /* String for the name of this Marae*/
     val Name: String,
-    val Search: String,
-    val Source: String,
-    val TKM_MID: Int,
-    val TPK_Region: String,
-    val Type: String,
+    /* String for the name of the Wharenui belonging to this Marae*/
     val Wharenui: String,
     /** X coordinate of this Marae */
     val X: Double,
     /** Y coordinate of this Marae */
-    val Y: Double
+    val Y: Double,
+
+    // TODO possibly remove the stuff bellow, I don't think that we need them
+
+    val Type: String,
+    val Source: String,
+    val TKM_MID: Int,
+    val TPK_Region: String,
+    val Search: String,
+    val GIS_MID: Int,
+    val Feedback: Any,
+    val Alternate: Any,
+    val Comments: Any,
 )
