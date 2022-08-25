9637e4c982306f0d71dc46e50b8c7d1797b126d6
chaka160 <kchay@windowslive.com>
2022-08-26 11:32:29 +1200

    Info Fragment now has the correct view for changing settings.

--- app/src/main/java/com/example/maps/ui/InfoFragment.kt
+++ app/src/main/java/com/example/maps/ui/InfoFragment.kt
@@ -2,12 +2,7 @@ package com.example.maps.ui
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.preference.PreferenceFragmentCompat
 import com.example.maps.R
 import com.example.maps.ui.placeholder.PlaceholderContent
 
@@ -16,49 +11,9 @@ import com.example.maps.ui.placeholder.PlaceholderContent
  * @author Kavan Chay
  */
 
-class InfoFragment : Fragment() {
+class InfoFragment : PreferenceFragmentCompat() {
 
-    private var columnCount = 1
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        arguments?.let {
-            columnCount = it.getInt(ARG_COLUMN_COUNT)
-        }
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_info_list, container, false)
-
-        // Set the adapter
-        if (view is RecyclerView) {
-            with(view) {
-                layoutManager = when {
-                    columnCount <= 1 -> LinearLayoutManager(context)
-                    else -> GridLayoutManager(context, columnCount)
-                }
-                adapter = MyInfoRecyclerViewAdapter(PlaceholderContent.ITEMS)
-            }
-        }
-        return view
-    }
-
-    companion object {
-
-        // TODO: Customize parameter argument names
-        const val ARG_COLUMN_COUNT = "column-count"
-
-        // TODO: Customize parameter initialization
-        @JvmStatic
-        fun newInstance(columnCount: Int) =
-            InfoFragment().apply {
-                arguments = Bundle().apply {
-                    putInt(ARG_COLUMN_COUNT, columnCount)
-                }
-            }
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.root_preferences, rootKey)
     }
 }
