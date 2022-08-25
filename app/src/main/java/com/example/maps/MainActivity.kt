cba9b2d2a86155553121f8469884530180a8b55a
Hugo Phibbs <81545721+HugoPhibbs@users.noreply.github.com>
2022-08-25 13:49:52 +1200

    changes to MainActivity.kt
    restructuring main
    
    new method for a marae bundle
    
    maps fragment has a marae bundle on startup

--- app/src/main/java/com/example/maps/MainActivity.kt
+++ app/src/main/java/com/example/maps/MainActivity.kt
@@ -3,10 +3,6 @@ package com.example.maps
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.add
-import androidx.fragment.app.commit
-import androidx.fragment.app.replace
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -21,55 +17,44 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        if (savedInstanceState == null) {
-
-            val bufferedReader = InputStreamReader(assets.open("Marae.json")).buffered()
-            val maraeArray = getMaraeCollection(bufferedReader)
-            val bundle: Bundle = Bundle()
-
-            bundle.putParcelableArray("maraeArray", maraeArray)
-
-            supportFragmentManager.commit {
-                setReorderingAllowed(true)
-                add<MapsFragment>(R.id.nav_host_fragment_container, args = bundle)
-            }
-        }
-
-            supportFragmentManager.commit {
-                setReorderingAllowed(true)
-                add<NavHostFragment>(androidx.navigation.fragment.R.id.nav_host_fragment_container)
-            }
-
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
-
-            val navController = findNavController(R.id.nav_host_fragment_activity_main)
-
-            val appBarConfiguration = AppBarConfiguration(
-                setOf(
-                    R.id.navigation_maps, R.id.navigation_wiki, R.id.navigation_settings
-                )
-            )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        setupNav(createMaraeListBundle());
     }
 
     /**
-     * Switches to the wiki fragment as per a user's request
+     * Sets up the navigation component of the main activity
+     *
+     * @param maraeListBundle Bundle containing an arrayList of marae to be passed as arguments
      */
-    fun switchToWikiFragment() {
-        // TODO generalise this method?
-        supportFragmentManager.commit {
-            replace<WikiFragment>(R.id.map_fragment_container_view)
-            setReorderingAllowed(true)
-            addToBackStack(null)// TODO set a name?
-        }
+    private fun setupNav(maraeListBundle : Bundle) {
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation_view)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+        navController.setGraph(R.navigation.nav_graph, maraeListBundle);
+
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.mapsFragment, R.id.wikiFragment, R.id.infoFragment))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        bottomNavigationView.setupWithNavController(navController)
     }
 
-    fun getMaraeCollection(bufferedReader : BufferedReader): Array<Marae> {
+    /**
+     * Creates a Bundle object for passing a list of marae between fragments
+     *
+     * @return Bundle object as described
+     */
+    private fun createMaraeListBundle() : Bundle {
+        // TODO use saved instance
+        val bufferedReader = InputStreamReader(assets.open("Marae.json")).buffered()
+        val maraeList = getMaraeList(bufferedReader)
+        val bundle = Bundle()
+        bundle.putParcelableArrayList("maraeList", maraeList)
+        return bundle
+    }
 
+    fun getMaraeList(bufferedReader : BufferedReader): ArrayList<Marae> {
         val jsonString = bufferedReader.use(BufferedReader::readText)
-
-        val arrayMaraeType = object : TypeToken<Array<Marae>>() {}.type
+        val arrayMaraeType = object : TypeToken<ArrayList<Marae>>() {}.type
         return Gson().fromJson(jsonString, arrayMaraeType)
     }
 }
