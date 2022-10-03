ef0434b4ce6bfa122ffb45799f0d95275f470bbb
Hugo Phibbs <81545721+HugoPhibbs@users.noreply.github.com>
2022-10-03 16:49:14 +1300

    getting language switching working,
    finally

--- app/src/main/java/com/example/maps/ui/SettingsFragment.kt
+++ app/src/main/java/com/example/maps/ui/SettingsFragment.kt
@@ -21,17 +21,11 @@ import java.util.*
  */
 class SettingsFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        //updateLocale("mi")
-        // Inflate the layout for this fragment
-
         return inflater.inflate(R.layout.fragment_settings, container, false)
     }
 
